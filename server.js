b82403d3e0b5dbf6459c60fbd3ca85dbc0f1d5ca
Alexander Paschenko <apaschenko@productengine.com>
2017-01-24 22:33:15 +0200

    Add server

---
+++ server.js
@@ -0,0 +1,25 @@
+
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.config');
+const express = require('express');
+const app = new express();
+const port = 3000;
+
+const compiler = webpack(config);
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+app.use(webpackHotMiddleware(compiler));
+app.use(express.static('resources'));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + '/index.html')
+});
+
+app.listen(port, function(error) {
+  if (error) {
+    console.error(error)
+  } else {
+    console.info("Server listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+  }
+});
